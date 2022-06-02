8509c2b8ef6e53d2a22deed412a23b2b8e0f54b0
wwq <342484594@qq.com>
2022-06-02 16:27:02 +0800

    首页排版

--- src/Weather.js
+++ src/Weather.js
@@ -1,7 +1,42 @@
+import {react,useState} from 'react'
+import { morning,afternoon,night } from './background'
+import './weather.css'
+
+
+
 function Weather() {
+  const [list, setList] = useState([{date:'TOMORROW',t:'28℃'},{date:'TOMORROW',t:'28℃'},{date:'TOMORROW',t:'28℃'}]);
     return (
-      <div className="Weather">
-          首页
+      <div className="Weather" style={morning}>
+         <header className='header'>
+           <div>
+              <h3>{'陕西省.西安'}</h3>
+              <div className='header-date'>
+                <span className='header-week'>TODAY</span>
+                <span>2022.6.1</span>
+              </div>
+           </div>
+           <div className='header-icon'>
+              icon
+           </div>
+         </header>
+          <main  class="main">
+              main
+          </main>
+          <footer>
+            <ul>
+              {
+                list && list.map((item,index)=>{
+                  return(
+                    <li className='list-text'>
+                      <p>{item.date}</p>
+                      <p>{item.t}</p>
+                    </li> 
+                  )
+                })
+              }
+              </ul>
+          </footer>
       </div>
     );
   }
---
+++ src/background.js
@@ -0,0 +1,18 @@
+
+export const morning = {
+    backgroundImage: `linear-gradient(to top, #fbc2eb 0%, #a6c1ee 100%)`
+} 
+
+export const afternoon = {
+    backgroundBlendMode:`normal, normal, screen, overlay, normal`,
+    background:`radial-gradient(ellipse farthest-side at 76% 77%, rgba(245, 228, 212, 0.25) 4%, rgba(255, 255, 255, 0)), 
+    radial-gradient(circle at 76% 40%, #fef6ec 4%, rgba(255, 255, 255, 0) 4.18%), 
+    linear-gradient(135deg, #ff0000 0%, #000036 100%), 
+    radial-gradient(ellipse at 28% 0%, #ffcfac 0%, rgba(98, 149, 144, 0.5) 100%), 
+    linear-gradient(180deg, #cd6e8a 0%, #f5eab0 69%, #d6c8a2 70%, #a2758d 100%)`,
+} 
+
+
+export const night = {
+    backgroundImage: `linear-gradient(to top, #30cfd0 -7%, #330867 70%)`
+}
